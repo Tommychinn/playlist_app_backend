3d775cb9cfca2fcef6ef64f9b16d9e0f65cfc953
Tommmychinn <tommychinn1@gmail.com>
2020-06-19 14:45:50 +0100

    get individual song from database

--- routes/songs.js
+++ routes/songs.js
@@ -17,23 +17,23 @@ router.get("/", (req, res) => {
     );
 });
 
-// GET individual song from database, again sort this out later
+// GET individual song from database
 
-// router.get("/:id", (req, res) => {
-//     const id = req.params.id;
-//     res.send(
-//         `This song is: ${connection.query(
-//             'SELECT ?, title, artist FROM track', id,
-//             (error, results) => {
-//                 if (error) {
-//                     console.log(`error accessing database: ${error}`);
-//                 } else {
-//                     res.send( results );
-//                 }
-//             }
-//         )}`
-//     );
-// });
+router.get("/:id", (req, res) => {
+    const id = req.params.id;
+    connection.query(
+        "SELECT id, title, artist FROM track WHERE id=?", id,
+        (error, results) => {
+            if (error) {
+                console.log(`error accessing database: ${error}`);
+            } else {
+                res.send({ results });
+            }
+        }
+    );
+});
+
+// POST song into database
 
 router.post("/", (req, res) => {
     const title = req.body.title;
@@ -46,19 +46,18 @@ router.post("/", (req, res) => {
     );
 });
 
+//UPDATE songs
 
-// Update a song, sort this out later
-
-// router.put("/:id", (req, res) => {
-//     const title = req.body.title;
-//     const artist = req.body.artist;
-//     const album_picture = req.body.album_picture;
-//     const youtube_url = req.body.youtube_url;
-//     const id = req.params.id;
-//     connection.query(
-//         `UPDATE TABLE track SET title = ? , artist = ? , album_picture = ? , youtube_url = ? WHERE id = ? `,
-//         [title, artist, album_picture, youtube_url, id]
-//     );
-// });
+router.put("/:id", (req, res) => {
+    const title = req.body.title;
+    const artist = req.body.artist;
+    const album_picture = req.body.album_picture;
+    const youtube_url = req.body.youtube_url;
+    const id = req.params.id;
+    connection.query(
+        "UPDATE track SET title = ? , artist = ?, album_picture = ?, youtube_url = ? WHERE id = ?",
+        [title, artist, album_picture, youtube_url, id]
+    );
+});
 
 module.exports = router;
